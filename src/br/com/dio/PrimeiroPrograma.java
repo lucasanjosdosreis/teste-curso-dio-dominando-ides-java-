221faffd3c07e0cbadfd64b923d9933c35c43a5a
lucas <lucasanjosdosreis@yahoo.com>
2021-12-01 20:51:03 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 8;
+        int c = 9;
+        int d = 10;
+        int e = 11;
+        int f = 12;
+        System.out.println("Hello World!" + (a+b+c+d+e+f));
+    }
+}
